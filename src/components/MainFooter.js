8161e5a314b4d2efeabd73d85e917330d440f413
Dan Brioli <technicaldebtgames@gmail.com>
2020-09-29 15:08:44 -0400

    :speech_balloon: Added use text

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -6,6 +6,7 @@ function MainFooter() {
   return (
     <footer className='MainFooter'>
       <h3>Copyright Dan Brioli (@techdebtgames) 2020-</h3>
+      <p>Use these however you like.</p>
     </footer>
   );
 }
